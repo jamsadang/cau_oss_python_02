0e0450c40f73298e975c9564811665168ccdf7c9
Hyunji Park <jamsadang588@gmail.com>
2024-04-08 00:09:09 +0900

    added volume_calc.py & added simple_calculator.py

---
+++ volume_calc.py
@@ -0,0 +1,8 @@
+width = float(input("가로:"))
+legth = float(input("세로:"))
+height = float(input("높이:"))
+
+total_volume = width * legth * height
+print("박스의 부피는", total_volume, "입니다.")
+
+print("Hello World!")
